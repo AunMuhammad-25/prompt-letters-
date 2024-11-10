c5b19ae59b99c8bed333ee0d3bc49f682bed3775
qamberali <qamberali4904@gmail.com>
2024-11-10 16:17:39 +0500

    promt(letter)

---
+++ script.js
@@ -0,0 +1,7 @@
+var word=prompt("enter one alphabet");
+if(word == "a" || word == "e" || word=="i" || word=="o" || word=="u"){
+    alert("it is a vowel letter")
+
+}else(
+    alert("is is consonat letter")
+)
